75849932cf5726c9f0897de59cd9c9f4afa5160c
Muhammad Zuhaily <2019705303@isiswa.uitm.edu.my>
2021-05-17 11:57:16 +0800

    Guest Create Complaint: configuring ajax controller for dropdown dependent

---
+++ app/Http/Controllers/AjaxController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\State;
+use App\Models\City;
+use App\Models\District;
+use App\Models\Panel;
+
+class AjaxController extends Controller
+{
+    // public function index()
+    // {
+    //     $data['countries'] = Country::get(["name","id"]);
+    //     return view('welcome', $data);
+    // }
+
+    public function getState(Request $request)
+    {
+        $data['states'] = State::where("country_id", $request->country_id)->get(["name","id"]);
+
+        return response()->json($data);
+    }
+
+    public function getCity(Request $request)
+    {
+        $data['cities'] = City::where("state_id", $request->state_id)->get(["name","id"]);
+
+        return response()->json($data);
+    }
+}
