9a0f7abe413fe99085e3c723855f2db4326da0fa
Zuhaily98 <2019705303@isiswa.uitm.edu.my>
2021-05-25 16:47:49 +0800

    dashboard: configure dashboard view

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Complaint;
 
 class DashboardController extends Controller
 {
     public function index()
-    {
-        return view('admin.dashboard');
+    { 
+        $pending = Complaint::where(['status' => 'Pending'])->count();
+        $closed = Complaint::where(['status' => 'Closed'])->count();
+        $low = Complaint::where(['priority' => 'Low'])->count();
+        $high = Complaint::where(['priority' => 'High'])->count();
+
+        return view('admin.dashboard')
+            ->with('pending', $pending)
+            ->with('closed', $closed)
+            ->with('low', $low)
+            ->with('high', $high);
     }
 }
--- app/Http/Controllers/Officer/DashboardController.php
+++ app/Http/Controllers/Officer/DashboardController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers\Officer;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Complaint;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('officer.dashboard');
+        $pending = Complaint::where(['status' => 'Pending'])->count();
+        $closed = Complaint::where(['status' => 'Closed'])->count();
+        $low = Complaint::where(['priority' => 'Low'])->count();
+        $high = Complaint::where(['priority' => 'High'])->count();
+
+        return view('officer.dashboard')
+            ->with('pending', $pending)
+            ->with('closed', $closed)
+            ->with('low', $low)
+            ->with('high', $high);
     }
 }
